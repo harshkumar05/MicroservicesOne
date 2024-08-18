56c1f6bd6792910d763671ccb5e67c22cba9664e
harshkumar05 <hk923458@gmail.com>
2024-08-18 18:10:08 +0530

    Setting up the service register

---
+++ service-register/src/test/java/com/msone/service_register/ServiceRegisterApplicationTests.java
@@ -0,0 +1,14 @@
+package com.msone.service_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Test line");
+	}
+
+}
